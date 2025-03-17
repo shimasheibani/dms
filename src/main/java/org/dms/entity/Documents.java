a556515c450f51445509c21f57a92e839986d9c3
shimasheibani <shima.sheibani72@gmail.com>
2025-03-17 12:18:42 +0100

    initial commit

---
+++ src/main/java/org/dms/entity/Documents.java
@@ -0,0 +1,34 @@
+package org.dms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.dms.enums.DocumentStatus;
+
+import java.util.Date;
+
+@Entity
+@Table(name="documents")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Documents {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+
+    @Column(name="upload_date")
+    private Date uploadDate;
+
+    @Enumerated(EnumType.STRING)
+    private DocumentStatus status;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private Users user;
+}
